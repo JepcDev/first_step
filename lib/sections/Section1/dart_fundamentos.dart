9957ca6e309668d82a1fa247519d9ded6b7ec6c7
JepcDev <joseenrique.pc21@gmail.com>
2025-01-22 00:09:03 -0400

    feat: add funciones y ejemplos de las funciones

---
+++ lib/sections/Section1/dart_fundamentos.dart
@@ -0,0 +1,42 @@
+
+// ANCHOR -> Variables y constantes
+// variables: Espacios en memoria para almacenar un valor, puede cambuiar a lo largo de la ejecucicion del programa.
+// Constantes: Son variables pero su contenido no cambia a lo largo de la ejecucion del programa.
+void main(List<String> args) {
+
+  //DEV:TODO -> variables y constantes
+  // int edad= 28;
+  // final double pi = 3.1416;
+
+  // print(saludos("Jose"));
+  saludos("Jose");
+}
+
+  // DEV:TODO -> funcion
+  // DEV:COMMENT -> sin retorno
+  // void saludos(String nombre) {
+  //   print("Bienvenido $nombre");
+  // }
+
+  // DEV:COMMENT -> con retorno
+  // String saludos(String nombre) {
+  //  print("Bienvenido $nombre");
+  //   return ("Bienvenido $nombre");
+  // }
+
+  // DEV:COMMENT -> function flecha
+  // void saludos(String nombre) => print("Bienvenido $nombre");
+
+
+  // DEV:TODO -> funcion con parametros opcionales  pocionales
+  // DEV:TODO -> funcion con parametros opcionales nombrados
+  // DEV:TODO -> programacion orientada a objetos
+  // DEV:TODO -> clases
+  // DEV:TODO -> constructores
+  // DEV:TODO -> Herencia
+  // DEV:TODO -> sobreescribir metodos
+  // DEV:TODO -> herencia constructor
+  // DEV:TODO -> clases abstractas
+  // DEV:TODO -> clases interfaces
+
+
