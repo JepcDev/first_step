f59537ddb4322389aa11177066227bad2ecda081
JepcDev <joseenrique.pc21@gmail.com>
2025-02-06 09:20:25 -0400

    feat: add classes

--- lib/sections/Section1/dart_fundamentos.dart
+++ lib/sections/Section1/dart_fundamentos.dart
@@ -9,7 +9,7 @@ void main(List<String> args) {
   // final double pi = 3.1416;
 
   // print(saludos("Jose"));
-  saludos("Jose", 'pc', 29);
+  // saludos("Jose", 'pc', 29);
 }
 
   // DEV:TODO -> funcion
@@ -28,17 +28,51 @@ void main(List<String> args) {
   // void saludos(String nombre) => print("Bienvenido $nombre");
 
   // DEV:TODO -> funcion con parametros opcionales  pocionales
-  void saludos(String nombre, [String? apellido, num? edad]){
-    if (apellido != null && edad != null) {
-      print('Bienvenido $nombre $apellido, edad $edad');
-    }else{
-      print('Bienvenido $nombre');
-    }
-  }
+  // void saludos(String nombre, [String? apellido, num? edad]){
+  //   if (apellido != null && edad != null) {
+  //     print('Bienvenido $nombre $apellido, edad $edad');
+  //   }else{
+  //     print('Bienvenido $nombre');
+  //   }
+  // }
 
   // DEV:TODO -> funcion con parametros opcionales nombrados
+  // void saludos({String nombre="Anonymous", String apellido = ""}) {
+  //   var saludos = StringBuffer("Bienvenido a Dart");
+  //   if (nombre != null) {
+  //     saludos.write(" $nombre ");
+  //   }
+  //   if (apellido != null) {
+  //     saludos.write(" $apellido ");
+  //   }
+  //   print(saludos.toString());
+  // }
+
   // DEV:TODO -> programacion orientada a objetos
+  // utiliza objetos para relacionarse, ejemplo: un automovil -> equema de poo, el automovil es elemento objeto principal que tiene una serie de (caracteristicas(propiedades):color,modelo,marca) y tambien tiene una serie de (funcionalidade(metodos):Arrancar,Detener,Estacionarse)
+
   // DEV:TODO -> clases
+  // ejemplo: tenemos objetos: automovil, camion y moto, los cuales comparten caracteristicas y funcionalidades similares, y si nos quisieramos referir a todos ellos como uno solo nos refeririamos como vehiculos
+  // Una clase es un conjunto de objetos que comparten caracteristicas y funcionalidades  comunes.
+  // class Vehiculo {
+    // caracteristicas - Propiedades
+  //   String? color;
+  //   String? modelo;
+  //   String? marca;
+
+    // funcionalidades - Metodos
+  //   void Arrancar(){
+  //     print("Hola soy el auto $marca de color $color y estoy arrancando");
+  //   }
+  // }
+
+  // void main(List<String> args) {
+  // var auto = Vehiculo();
+  // auto.color = "Rojo";
+  // auto.marca = "Ford";
+  // auto.Arrancar();
+  // }
+
   // DEV:TODO -> constructores
   // DEV:TODO -> Herencia
   // DEV:TODO -> sobreescribir metodos
