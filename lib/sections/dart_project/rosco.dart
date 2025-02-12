3e65ed8c1b174901a0bfa22174863d20a44e8ba3
JepcDev <joseenrique.pc21@gmail.com>
2025-02-12 18:37:26 -0400

    feat: add construtor and generate static values for game rosco

--- lib/sections/dart_project/rosco.dart
+++ lib/sections/dart_project/rosco.dart
@@ -2,7 +2,38 @@ import 'pregunta.dart';
 
 // Clase del juego del Rosco
 class Rosco{
-  List<Pregunta>? roscoPreguntas;
+  List<Pregunta>? roscoPreguntas ;
+
+  static List letras = const ["A", "B", "C", "D", "E", "F"];
+  static List definiciones = const [
+    "Persona que tripula una Astronave o que está entrenada para este Trabajo",
+    "Especie de Talega o Saco de Tela y otro material que sirve para llevar o guardar algo",
+    "Aparato destinado a registrar imágenes animadas para el cine o la telivision",
+    "Obra literaria escrita para ser representada",
+    "Que se prolonga muchisimo o excesivamente",
+    "Laboratorio y despacho del farmaceutico"
+  ];
+  static List respuestas = [
+    "Astronauta",
+    "Bolsa",
+    "Camara",
+    "Drama",
+    "Eterno",
+    "Farmacia"
+  ];
+
+  Rosco(){
+    for (var index = 0; index < letras.length; index++) {
+      Pregunta roscoPregunta = Pregunta(letras[index], definiciones[index], respuestas[index]);
+      roscoPreguntas!.add(roscoPregunta);
+    }
+
+    // for (var letra in letras) {
+    //   var index = letras.indexOf(letra);
+    //   var roscoPregunta = Pregunta(letra, definiciones[index], respuestas[index]);
+    //   roscoPreguntas!.add(roscoPregunta);
+    // }
+  }
 
   Pregunta obtenerPregunta(){
     return Pregunta("","","");
