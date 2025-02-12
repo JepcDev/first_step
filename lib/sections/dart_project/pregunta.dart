599d0946935181f7b76ed603e0270beb06583ea9
JepcDev <joseenrique.pc21@gmail.com>
2025-02-12 18:10:45 -0400

    feat: add implement class atributes methods and constructors Rosco, Preguntas

---
+++ lib/sections/dart_project/pregunta.dart
@@ -0,0 +1,10 @@
+
+// Clae que contiene la estructura de la spreguntas
+class Pregunta{
+
+  String letra;
+  String definicion;//contenido de la pregunta
+  String respuesta;
+
+  Pregunta(this.letra, this.definicion, this.respuesta);
+}
---
+++ lib/sections/dart_project/rosco.dart
@@ -0,0 +1,21 @@
+import 'pregunta.dart';
+
+// Clase del juego del Rosco
+class Rosco{
+  List<Pregunta>? roscoPreguntas;
+
+  Pregunta obtenerPregunta(){
+    return Pregunta("","","");
+  }
+
+  Pregunta pasaPalbra(){
+    return Pregunta("","","");
+  }
+
+  String evaluarRepsuesta(){
+    return " ";
+  }
+
+
+
+}
